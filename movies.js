e57bf7dd796615172a5b0d079c93595efbc7d71b
Code-maker0 <watchoffline2022+github@gmail.com>
2025-03-01 23:35:27 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,12 +1,13 @@
 // Replace with your TMDB API key
 const apiKey = 'b31550cf1a6a31c5eb1fe12f3afc44f0';
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // CORS proxy
 let currentPage = 1;
 let totalPages = 1;
 let currentGenre = '';
 
 // Fetch genres from TMDB
 async function fetchGenres() {
-  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
+  const url = `${proxyUrl}https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
   const response = await fetch(url);
   const data = await response.json();
   const genreSelect = document.getElementById('genre-filter');
@@ -21,8 +22,8 @@ async function fetchGenres() {
 // Fetch movies from TMDB
 async function fetchMovies(page = 1, genre = '') {
   const url = genre
-    ? `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&page=${page}&with_genres=${genre}`
-    : `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`;
+    ? `${proxyUrl}https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&page=${page}&with_genres=${genre}`
+    : `${proxyUrl}https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`;
   const response = await fetch(url);
   const data = await response.json();
   totalPages = data.total_pages;
@@ -50,7 +51,7 @@ function displayMovies(movies) {
 
 // Show trailer
 async function showTrailer(movieId) {
-  const url = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}&language=en-US`;
+  const url = `${proxyUrl}https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}&language=en-US`;
   const response = await fetch(url);
   const data = await response.json();
   const trailer = data.results.find(video => video.type === 'Trailer');
@@ -95,7 +96,7 @@ async function searchMovie() {
     alert('Please enter a movie title!');
     return;
   }
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
+  const url = `${proxyUrl}https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
   const response = await fetch(url);
   const data = await response.json();
   displayMovies(data.results);
